4ee9e096bfd9b0bf6a8b5e58e18af4412073e2bc
andreib04 <andrei_raul04@yahoo.com>
2026-04-11 22:24:42 +0300

    feat: add global exception handling middleware

---
+++ JobsTracker.API/JobsTracker.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace JobsTracker.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+
+            var statusCode = exception switch
+            {
+                UnauthorizedAccessException => HttpStatusCode.Unauthorized,
+                ArgumentException => HttpStatusCode.BadRequest,
+                KeyNotFoundException => HttpStatusCode.NotFound,
+                _ => HttpStatusCode.InternalServerError
+            };
+
+            response.StatusCode = (int)statusCode;
+
+            var result = JsonSerializer.Serialize(new
+            {
+                error = exception.Message,
+                statusCode = response.StatusCode
+            });
+
+            return response.WriteAsync(result);
+        }
+    }
+}
